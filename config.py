5eabfaad1679ae7d1c52eeb4ee90ae3c7e78e61d
ShibaDeku29 <quyenpvv6522@ut.edu.vn>
2025-05-29 19:42:48 +0700

    Add safe config.py using environment variables

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-git push --force
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
