e5a186a27606b4b5fcabb5e87f8fc091df8cb1c8
ShibaDeku29 <quyenpvv6522@ut.edu.vn>
2025-05-29 19:52:34 +0700

    Remove all __pycache__ and .pyc files containing secrets

--- models/message.py
+++ models/message.py
@@ -1,6 +1,9 @@
 from extensions import db
+# Import User nếu cần reference cho ForeignKey (import sau này nếu cần)
+# from .user import User
 
-class User(db.Model):
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
